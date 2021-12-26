bea50b1cd6c7cea3e6b0e0462b3f5df227401c93
Mark Howard <markhoward2005@gmail.com>
2021-12-26 20:34:55 +0000

    Added UI Template Classic Series 19-26 And New Series 1-5

--- Source Code/DW Episode Guide/DW Episode Guide/ClassicSeries/ClassicSeries24/Dragonfire.swift
+++ Source Code/DW Episode Guide/DW Episode Guide/ClassicSeries/ClassicSeries24/Dragonfire.swift
@@ -17,7 +17,146 @@ struct Dragonfire: View {
         @State var showingShare = false
         @AppStorage("DragonfireNotes") var notes = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ForEach(items) { item in
+                    ScrollView {
+                        HStack {
+                            Spacer()
+                            Image("")
+                                .resizable()
+                                .scaledToFill()
+                                .cornerRadius(25)
+                                .frame(width: 150, height: 150)
+                                .contextMenu {
+                                    Button(action: {let pasteboard = NSPasteboard.general
+                                        pasteboard.clearContents()
+                                        pasteboard.writeObjects([NSImage(named: "")!])
+                                    }) {
+                                        Text("Copy")
+                                    }
+                                }
+                            Spacer()
+                            VStack {
+                            Text("\(item.title!)")
+                                    .bold()
+                                    .font(.title)
+                                    .padding()
+                            Text("Story No. ")
+                                    .font(.title3)
+                                Text("Written By - ")
+                                    .font(.title3)
+                        }
+                            Spacer()
+                        }
+                        .padding()
+                        Divider()
+                        HStack {
+                            Spacer()
+                            GroupBox(label: Label("Broadcast", systemImage: "dot.radiowaves.left.and.right")) {
+                                VStack {
+                                    Spacer()
+                                    HStack {
+                                        Spacer()
+                                Text("\(item.broadcast!)")
+                                        Spacer()
+                                    }
+                                    Spacer()
+                                }
+                            }
+                            Spacer()
+                            GroupBox(label: Label("Companions", systemImage: "person.2.fill")) {
+                                VStack {
+                                    Spacer()
+                                    HStack {
+                                        Spacer()
+                                Text("\(item.companions!)")
+                                        Spacer()
+                                    }
+                                    Spacer()
+                                }
+                            }
+                            Spacer()
+                        }
+                        .padding()
+                        Divider()
+                        HStack {
+                            Spacer()
+                            GroupBox(label: Label("Director", systemImage: "camera.fill")) {
+                                VStack {
+                                    Spacer()
+                                    HStack {
+                                        Spacer()
+                                Text("\(item.director!)")
+                                        Spacer()
+                                    }
+                                    Spacer()
+                                }
+                            }
+                            Spacer()
+                            GroupBox(label: Label("Producer", systemImage: "person.text.rectangle")) {
+                                VStack {
+                                    Spacer()
+                                    HStack {
+                                        Spacer()
+                                Text("\(item.producer!)")
+                                        Spacer()
+                                }
+                                    Spacer()
+                                }
+                            }
+                            Spacer()
+                        }
+                        .padding()
+                        Divider()
+                        HStack {
+                            Spacer()
+                            GroupBox(label: Label("Doctor", systemImage: "person.crop.square.filled.and.at.rectangle")) {
+                                VStack {
+                                    Spacer()
+                                    HStack {
+                                        Spacer()
+                                Text("\(item.doctor!)")
+                                        Spacer()
+                                }
+                                    Spacer()
+                                }
+                            }
+                            Spacer()
+                            GroupBox(label: Label("Length", systemImage: "clock.arrow.circlepath")) {
+                                VStack {
+                                    Spacer()
+                                    HStack {
+                                        Spacer()
+                                Text("\(item.length!)")
+                                        Spacer()
+                                    }
+                                    Spacer()
+                                }
+                            }
+                            Spacer()
+                        }
+                        .padding()
+                        Divider()
+                        HStack {
+                            Spacer()
+                            GroupBox(label: Label("Notes", systemImage: "note.text")) {
+                            TextEditor(text: $notes)
+                                    .frame(height: 200)
+                            }
+                            Spacer()
+                        }
+                        .padding()
+                    }
+                    .toolbar {
+                        ToolbarItem(placement: .primaryAction) {
+                            Button(action: {self.showingShare = true}) {
+                                Image(systemName: "square.and.arrow.up")
+                            }
+                            .background(SharingsPicker(isPresented: $showingShare, sharingItems: [URL(string: "")!]))
+                        }
+                    }
+                    .navigationTitle("\(item.title!)")
+            }
+
     }
 }
 
