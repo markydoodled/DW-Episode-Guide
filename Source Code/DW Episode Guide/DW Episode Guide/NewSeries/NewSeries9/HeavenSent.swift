c02c556604a63155d457621dce46745ea1cbf4ec
Mark Howard <markhoward2005@gmail.com>
2022-04-14 08:47:50 +0100

    Added Base iOS Views Classic And New Series

--- Source Code/DW Episode Guide/DW Episode Guide/NewSeries/NewSeries9/HeavenSent.swift
+++ Source Code/DW Episode Guide/DW Episode Guide/NewSeries/NewSeries9/HeavenSent.swift
@@ -171,7 +171,261 @@ struct HeavenSent: View {
             .navigationTitle("\(item.title!)")
     }
     #elseif os(iOS)
-
+        if horizontalSizeClass == .compact {
+                ForEach(items) { item in
+                Form {
+                    HStack {
+                        Spacer()
+                        Image("")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 150, height: 150)
+                            .contextMenu {
+                                Button(action: {let pasteboard = UIPasteboard.general
+                                    pasteboard.image = UIImage(named: "")
+                                }) {
+                                    Label("Copy", systemImage: "doc.on.doc")
+                                }
+                            }
+                            .onDrag {
+                                return NSItemProvider(object: UIImage(named: "")! as UIImage)
+                            }
+                        Spacer()
+                    }
+                    Text("Story No. ")
+                    .onDrag {
+                        return NSItemProvider(object: String("Story No. ") as NSString)
+                    }
+                    Text("Written By - ")
+                        .onDrag {
+                            return NSItemProvider(object: String("Written By - ") as NSString)
+                        }
+                    Section(header: Label("Broadcast", systemImage: "dot.radiowaves.left.and.right")) {
+                        Text("\(item.broadcast!)")
+                            .onDrag {
+                                return NSItemProvider(object: String("\(item.broadcast!)") as NSString)
+                            }
+                    }
+                    Section(header: Label("Companions", systemImage: "person.2.fill")) {
+                        Text("\(item.companions!)")
+                            .onDrag {
+                                return NSItemProvider(object: String("\(item.companions!)") as NSString)
+                            }
+                    }
+                    Section(header: Label("Director", systemImage: "camera.fill")) {
+                        Text("\(item.director!)")
+                            .onDrag {
+                                return NSItemProvider(object: String("\(item.director!)") as NSString)
+                            }
+                    }
+                    Section(header: Label("Producer", systemImage: "person.text.rectangle")) {
+                        Text("\(item.producer!)")
+                            .onDrag {
+                                return NSItemProvider(object: String("\(item.producer!)") as NSString)
+                            }
+                    }
+                    Section(header: Label("Doctor", systemImage: "person.crop.square.filled.and.at.rectangle")) {
+                        Text("\(item.doctor!)")
+                            .onDrag {
+                                return NSItemProvider(object: String("\(item.doctor!)") as NSString)
+                            }
+                    }
+                    Section(header: Label("Length", systemImage: "clock.arrow.circlepath")) {
+                        Text("\(item.length!)")
+                            .onDrag {
+                                return NSItemProvider(object: String("\(item.length!)") as NSString)
+                            }
+                    }
+                    Section(header: Label("Notes", systemImage: "note.text")) {
+                        TextEditor(text: $notes)
+                            .frame(height: 200)
+                    }
+                }
+                .textSelection(.enabled)
+                .navigationTitle("\(item.title!)")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button(action: {self.showingShare = true}) {
+                            Image(systemName: "square.and.arrow.up")
+                        }
+                        .sheet(isPresented: $showingShare) {
+                            ActivityView(activityItems: [URL(string: "")!], applicationActivities: nil)
+                        }
+                    }
+                }
+            }
+        } else {
+            ForEach(items) { item in
+                ScrollView {
+                    HStack {
+                        Spacer()
+                        Image("")
+                            .resizable()
+                            .scaledToFill()
+                            .cornerRadius(25)
+                            .frame(width: 150, height: 150)
+                            .contextMenu {
+                                Button(action: {let pasteboard = UIPasteboard.general
+                                    pasteboard.image = UIImage(named: "")
+                                }) {
+                                    Label("Copy", systemImage: "doc.on.doc")
+                                }
+                            }
+                            .onDrag {
+                                return NSItemProvider(object: UIImage(named: "")! as UIImage)
+                            }
+                        Spacer()
+                        VStack {
+                        Text("\(item.title!)")
+                                .bold()
+                                .font(.title)
+                                .padding()
+                        Text("Story No. ")
+                                .font(.title3)
+                                .onDrag {
+                                    return NSItemProvider(object: String("Story No. ") as NSString)
+                                }
+                            Text("Written By - ")
+                                .font(.title3)
+                                .onDrag {
+                                    return NSItemProvider(object: String("Written By - ") as NSString)
+                                }
+                    }
+                        Spacer()
+                    }
+                    .padding()
+                    Divider()
+                    HStack {
+                        Spacer()
+                        GroupBox(label: Label("Broadcast", systemImage: "dot.radiowaves.left.and.right")) {
+                            VStack {
+                                Spacer()
+                                HStack {
+                                    Spacer()
+                            Text("\(item.broadcast!)")
+                                    Spacer()
+                                }
+                                Spacer()
+                            }
+                        }
+                        .onDrag {
+                            return NSItemProvider(object: String("\(item.broadcast!)") as NSString)
+                        }
+                        Spacer()
+                        GroupBox(label: Label("Companions", systemImage: "person.2.fill")) {
+                            VStack {
+                                Spacer()
+                                HStack {
+                                    Spacer()
+                            Text("\(item.companions!)")
+                                    Spacer()
+                                }
+                                Spacer()
+                            }
+                        }
+                        .onDrag {
+                            return NSItemProvider(object: String("\(item.companions!)") as NSString)
+                        }
+                        Spacer()
+                    }
+                    .padding()
+                    Divider()
+                    HStack {
+                        Spacer()
+                        GroupBox(label: Label("Director", systemImage: "camera.fill")) {
+                            VStack {
+                                Spacer()
+                                HStack {
+                                    Spacer()
+                            Text("\(item.director!)")
+                                    Spacer()
+                                }
+                                Spacer()
+                            }
+                        }
+                        .onDrag {
+                            return NSItemProvider(object: String("\(item.director!)") as NSString)
+                        }
+                        Spacer()
+                        GroupBox(label: Label("Producer", systemImage: "person.text.rectangle")) {
+                            VStack {
+                                Spacer()
+                                HStack {
+                                    Spacer()
+                            Text("\(item.producer!)")
+                                    Spacer()
+                            }
+                                Spacer()
+                            }
+                        }
+                        .onDrag {
+                            return NSItemProvider(object: String("\(item.producer!)") as NSString)
+                        }
+                        Spacer()
+                    }
+                    .padding()
+                    Divider()
+                    HStack {
+                        Spacer()
+                        GroupBox(label: Label("Doctor", systemImage: "person.crop.square.filled.and.at.rectangle")) {
+                            VStack {
+                                Spacer()
+                                HStack {
+                                    Spacer()
+                            Text("\(item.doctor!)")
+                                    Spacer()
+                            }
+                                Spacer()
+                            }
+                        }
+                        .onDrag {
+                            return NSItemProvider(object: String("\(item.doctor!)") as NSString)
+                        }
+                        Spacer()
+                        GroupBox(label: Label("Length", systemImage: "clock.arrow.circlepath")) {
+                            VStack {
+                                Spacer()
+                                HStack {
+                                    Spacer()
+                            Text("\(item.length!)")
+                                    Spacer()
+                                }
+                                Spacer()
+                            }
+                        }
+                        .onDrag {
+                            return NSItemProvider(object: String("\(item.length!)") as NSString)
+                        }
+                        Spacer()
+                    }
+                    .padding()
+                    Divider()
+                    HStack {
+                        Spacer()
+                        GroupBox(label: Label("Notes", systemImage: "note.text")) {
+                        TextEditor(text: $notes)
+                                .frame(height: 200)
+                        }
+                        Spacer()
+                    }
+                    .padding()
+                }
+                .toolbar {
+                    ToolbarItem(placement: .primaryAction) {
+                        Button(action: {self.showingShare = true}) {
+                            Image(systemName: "square.and.arrow.up")
+                        }
+                        .sheet(isPresented: $showingShare) {
+                            ActivityView(activityItems: [URL(string: "")!], applicationActivities: nil)
+                        }
+                    }
+                }
+                .textSelection(.enabled)
+                .navigationTitle("\(item.title!)")
+                .navigationBarTitleDisplayMode(.inline)
+        }
+        }
     #endif
     }
 }
