d09926432d14c326f10abdd9fb5a8c454f4030a1
Mark Howard <markydoodled@users.noreply.github.com>
2022-04-17 21:08:19 +0100

    FocusState New Series 1

--- Source Code/DW Episode Guide/DW Episode Guide/NewSeries/NewSeries1/AliensOfLondon.swift
+++ Source Code/DW Episode Guide/DW Episode Guide/NewSeries/NewSeries1/AliensOfLondon.swift
@@ -19,6 +19,7 @@ struct AliensOfLondon: View {
         @AppStorage("AliensOfLondonNotes") var notes = ""
     #if os(iOS)
     @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    @FocusState private var isFocused: Bool
     #endif
     var body: some View {
         #if os(macOS)
--- Source Code/DW Episode Guide/DW Episode Guide/NewSeries/NewSeries1/Dalek.swift
+++ Source Code/DW Episode Guide/DW Episode Guide/NewSeries/NewSeries1/Dalek.swift
@@ -19,6 +19,7 @@ struct Dalek: View {
         @AppStorage("DalekNotes") var notes = ""
     #if os(iOS)
     @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    @FocusState private var isFocused: Bool
     #endif
     var body: some View {
         #if os(macOS)
